2fd6a90ba217b4a5986208c3e976ce8301315e8f
A-Mel45 <alexmele0507@gmail.com>
2026-04-13 22:04:16 -0400

    W.I.P part 4
    options menu done
    - need to fix the sliders
    pause menu set up
    - need code to turn on/off/ect..

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,6 +1,7 @@
 using System.Reflection;
 using UnityEngine;
 using UnityEngine.UIElements;
+using System.Collections.Generic;
 
 public class Main : MonoBehaviour
 {
@@ -13,6 +14,15 @@ public class Main : MonoBehaviour
     VisualElement mainMenu;
     VisualElement optionsMenu;
 
+    Slider activeSlider;
+    [SerializeField] float sliderValueFloat;
+    string sliderName;
+    string sliderValueString;
+    List<Slider> sliderList;
+    [SerializeField] float resetValue = 100;
+
+
+
     void Start()
     {
         root = GetComponent<UIDocument>().rootVisualElement;
@@ -22,11 +32,28 @@ public class Main : MonoBehaviour
         mainMenu = root.Query<VisualElement>("MainMenu");
         optionsMenu = root.Query<VisualElement>("OptionsMenu");
         optionsMenu.style.display = DisplayStyle.None;
+
+        root.RegisterCallback<ClickEvent>(ClickAction);
+        root.RegisterCallback<PointerOverEvent>(PointerOverAction);
+
+        sliderList = root.Query<Slider>().ToList();
+        foreach(Slider currentSlider in sliderList)
+        {
+            activeSlider = currentSlider;
+
+            currentSlider.value = PlayerPrefs.GetFloat(currentSlider.name, resetValue);
+            UpdateSliderLabel();
+
+            if(currentSlider.name == "Master_volume")
+            {
+                AudioListener.volume = currentSlider.value / 100;
+            }
+
+        }
     }
     void Update()
     {
-        root.RegisterCallback<ClickEvent>(ClickAction);
-        root.RegisterCallback<PointerOverEvent>(PointerOverAction);
+        
     }
 
     void ClickAction(ClickEvent eventInfo)
@@ -46,10 +73,12 @@ public class Main : MonoBehaviour
             else if(activeTarget.Contains("Settings"))
             {
                 optionsMenu.style.display = DisplayStyle.Flex;
+                root.RegisterCallback<ChangeEvent<float>>(ChangeAction);
             }
             else if(activeTarget.Contains("Close"))
             {
                 optionsMenu.style.display = DisplayStyle.None;
+                root.UnregisterCallback<ChangeEvent<float>>(ChangeAction);
             }
         }
         //Debug.Log(eventInfo);
@@ -65,15 +94,54 @@ public class Main : MonoBehaviour
         }
     }
 
+    void ChangeAction(ChangeEvent<float> eventInfo)
+    {
+        activeSlider = (Slider)eventInfo.target;
+        sliderValueFloat = activeSlider.value;
+
+        PlayerPrefs.SetFloat(activeSlider.name, activeSlider.value);
+
+        UpdateSliderLabel();
+
+        if (activeSlider.name == "Master_Volume")
+        {
+            AudioListener.volume = sliderValueFloat / 100;
+        }
+    }
+
     void QuitGame()
     {
         Application.Quit();
         Debug.Log("quit Game");
     }
 
+    void UpdateSliderLabel()
+    {
+        sliderValueString = activeSlider.value.ToString("0");
+        sliderName = activeSlider.name;
+        sliderName = sliderName.Replace("-", "/").Replace("_", " ");
+        sliderName = sliderName + ": " + sliderValueString + "%";
+
+        activeSlider.label = sliderName;
+    }
+
     public void OnExit()
     {
         optionsMenu.style.display = DisplayStyle.None;
+    }
+
+    public void OnReload()
+    {
+        foreach (Slider currentSlider in sliderList)
+        {
+            activeSlider = currentSlider;
+            
+            PlayerPrefs.SetFloat(currentSlider.name, resetValue);
+            currentSlider.value = resetValue;
+
+            UpdateSliderLabel();
+            AudioListener.volume = resetValue / 100;
+        }
     }
 
 }
